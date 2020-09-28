28ce92ef7aa1c43f1bcfbd24345305ab5e676de6
可愛いくく🐸kuku <kkukuli@tencent.com>
2020-09-28 21:13:18 +0800

    feat: table structure

--- src/components/BasicInformationTable.js
+++ src/components/BasicInformationTable.js
@@ -3,8 +3,10 @@ import {listBasics} from "../graphql/queries";
 import {useQuery} from 'react-apollo-hooks';
 import {gql} from "apollo-boost";
 
+import {Table, Tag, Space} from 'antd';
+
 export default function BasicInformationTable() {
-  const {loading, error, data} = useQuery(gql`${listBasics}`, {variables: {limit: 10}});
+  const {loading, error, data} = useQuery(gql`${listBasics}`, {variables: {limit: 300}});
 
   if (loading) {
     return <div>'loading....'</div>
@@ -14,5 +16,61 @@ export default function BasicInformationTable() {
     return <div>'error....'</div>
   }
 
+  const {items, nextToken} = data.listBasics;
 
+  const dataSource = items.map(item => ({...item, key: item.id}))
+
+  const columns = [
+    {
+      title: 'FIRST NAME',
+      dataIndex: 'firstName',
+      key: 'firstName',
+    },
+    {
+      title: 'LAST NAME',
+      dataIndex: 'lastName',
+      key: 'lastName',
+    },
+    {
+      title: 'COUNTRY',
+      key: 'countryResidence',
+      dataIndex: 'countryResidence',
+      render: tags => (
+        <span>
+        {tags.map(tag => {
+          return (
+            <Tag color={'geekblue'} key={tag}>
+              {tag.toUpperCase()}
+            </Tag>
+          );
+        })}
+      </span>
+      ),
+    },
+    {
+      title: 'CITY',
+      dataIndex: 'cityResidence',
+      key: 'cityResidence',
+    },
+    {
+      title: 'TITLE',
+      dataIndex: 'title',
+      key: 'title',
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: (text, record) => (
+        <Space size="middle">
+          <a>EDIT</a>
+        </Space>
+      ),
+    },
+  ];
+
+  return (
+    <div>
+      <Table columns={columns} dataSource={dataSource} pagination={{pageSize: 50}}/>
+    </div>
+  )
 }

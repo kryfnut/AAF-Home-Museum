2345de24e1b1817183b0852acf977b278ab0a81c
kryfnut <oluwatobi.solomon@gmail.com>
2024-04-15 14:54:20 +0100

    amplify cli update
    backend api update / creation of new api key

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,33 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "basic": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string"
+    }
+  },
+  "auth": {
+    "homemuseum352fda01": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "AppClientSecret": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "hosting": {
+    "S3AndCloudFront": {
+      "HostingBucketName": "string",
+      "Region": "string",
+      "S3BucketSecureURL": "string",
+      "WebsiteURL": "string"
+    }
+  },
+  "storage": {
+    "museum": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
